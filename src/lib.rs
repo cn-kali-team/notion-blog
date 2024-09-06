be5ac509806dd6476e45aa6fe592b9ed234864fb
cn-kali-team <kali-team@qq.com>
2024-09-06 13:55:52 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -427,7 +427,7 @@ fn rewriter(
                     // el.append(rewriter_http, ContentType::Html);
                     el.append(BASE, ContentType::Html);
                     el.append(REWRITER_HTTP, ContentType::Html);
-                    el.append(&update_history(&blog_env.page_map), ContentType::Html);
+                    // el.append(&update_history(&blog_env.page_map), ContentType::Html);
                     el.append(RESIZE, ContentType::Html);
                     el.append(THEME, ContentType::Html);
                     el.append(TOC, ContentType::Html);
